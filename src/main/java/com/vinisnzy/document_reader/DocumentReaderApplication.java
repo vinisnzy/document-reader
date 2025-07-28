49f57b5c612f2a5b09556018e36027164c7f2599
Vinícius de Paula <manoseilacara123@gmail.com>
2025-07-28 20:23:34 -0300

    initial commit

---
+++ src/main/java/com/vinisnzy/document_reader/DocumentReaderApplication.java
@@ -0,0 +1,13 @@
+package com.vinisnzy.document_reader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocumentReaderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DocumentReaderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vinisnzy/document_reader/DocumentReaderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vinisnzy.document_reader;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DocumentReaderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
